05daa0c50e5f90b12573f683b171ac9c483870e8
Tomás García <tgarrey@gmail.com>
2018-11-11 19:29:29 +0000

    First commit.

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <a 
+            className="App-link"
+            href="https://tgarrey.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            tgarrey.com
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
