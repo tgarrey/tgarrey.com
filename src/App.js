7f5c2aa5e54a833a90d22e1617464a8bd33bfb3d
Tomás García <tgarrey@gmail.com>
2018-11-16 21:11:09 +0000

    Clean react references.

--- src/App.js
+++ src/App.js
@@ -8,16 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+          <a className="App-link"
+             href="https://tgarrey.com"
+             rel="noopener noreferrer">
+            tgarrey.com
           </a>
         </header>
       </div>
